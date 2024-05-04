ba1bf168542d7074b7beb5d680a1964978509466
Lee HuiGwang <lhk9805@gmail.com>
2024-05-05 01:54:28 +0900

    fix: 이전 검색 결과가 잘못 보여지는 버그 해결
    - 검색 결과들을 flow에 태워서 발생했으므로, 이를 수정

--- app/src/main/java/com/winteryy/nbcsearch/domain/usecase/GetSearchImageUseCaseImpl.kt
+++ app/src/main/java/com/winteryy/nbcsearch/domain/usecase/GetSearchImageUseCaseImpl.kt
@@ -3,12 +3,11 @@ package com.winteryy.nbcsearch.domain.usecase
 import com.winteryy.nbcsearch.domain.entity.ContentItemEntity
 import com.winteryy.nbcsearch.domain.entity.ResultEntity
 import com.winteryy.nbcsearch.domain.entity.SearchImageEntity
+import com.winteryy.nbcsearch.domain.entity.StorageEntity
 import com.winteryy.nbcsearch.domain.repository.SearchRepository
 import com.winteryy.nbcsearch.domain.repository.StorageRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.combineTransform
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,6 +17,8 @@ class GetSearchImageUseCaseImpl @Inject constructor(
     private val storageRepository: StorageRepository
 ): GetSearchImageUseCase {
 
+    private var cachedSearchResult: SearchImageEntity? = null
+
     override suspend fun invoke(
         query: String,
         sort: String,
@@ -26,30 +27,34 @@ class GetSearchImageUseCaseImpl @Inject constructor(
     ): Flow<ResultEntity> {
 
         val favoriteFlow = storageRepository.getFavoriteItemMap()
-        val searchResultFlow = flow {
-            emit(
-                searchRepository.getSearchImage(
-                    query = query,
-                    sort = sort,
-                    page = page,
-                    size = size
-                )
-            )
-        }
 
-        return searchResultFlow.combine(favoriteFlow) { searchResult, favorites ->
-            ResultEntity(
-                meta = searchResult.meta,
-                contentItems = searchResult.documents?.map { imageDocumentEntity ->
-                    ContentItemEntity(
-                        thumbnailUrl = imageDocumentEntity.thumbnailUrl,
-                        displaySiteName = imageDocumentEntity.displaySiteName,
-                        datetime = imageDocumentEntity.datetime,
-                        isFavorite = favorites.containsKey(imageDocumentEntity.thumbnailUrl)
-                    )
-                }
-            )
+        val searchResult = searchRepository.getSearchImage(
+            query = query,
+            sort = sort,
+            page = page,
+            size = size
+        )
+        cachedSearchResult = searchResult
+
+        return favoriteFlow.map { favorites ->
+            updateSearchResultWithFavorites(favorites)
         }
+    }
+
+    private fun updateSearchResultWithFavorites(
+        favorites: HashMap<String, StorageEntity>
+    ): ResultEntity {
+        return ResultEntity(
+            meta = cachedSearchResult?.meta,
+            contentItems = cachedSearchResult?.documents?.map { imageDocumentEntity ->
+                ContentItemEntity(
+                    thumbnailUrl = imageDocumentEntity.thumbnailUrl,
+                    displaySiteName = imageDocumentEntity.displaySiteName,
+                    datetime = imageDocumentEntity.datetime,
+                    isFavorite = favorites.containsKey(imageDocumentEntity.thumbnailUrl)
+                )
+            }
+        )
     }
 
 }
